acc6fe8c1e5008588b428e3b7ca3be9c2804e90b
abruere <abruere@users.noreply.github.com>
2017-05-24 12:53:51 +0200

    Add excludeScope option to avoid serving cached index file when URL matches (#2)
    Allows use along with non ember pages on same domain.

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,7 +1,10 @@
 import {
   INDEX_HTML_PATH,
-  VERSION
+  VERSION,
+  INDEX_EXCLUDE_SCOPE
 } from 'ember-service-worker-index/service-worker/config';
+
+import { urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
 import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 
 const CACHE_KEY_PREFIX = 'esw-index';
@@ -27,9 +30,10 @@ self.addEventListener('fetch', (event) => {
   let request = event.request;
   let isGETRequest = request.method === 'GET';
   let isHTMLRequest = request.headers.get('accept').indexOf('text/html') !== -1;
-  let isLocal = new URL(request.url).origin === location.origin
+  let isLocal = new URL(request.url).origin === location.origin;
+  let scopeExcluded = urlMatchesAnyPattern(request.url, INDEX_EXCLUDE_SCOPE);
 
-  if (isGETRequest && isHTMLRequest && isLocal) {
+  if (isGETRequest && isHTMLRequest && isLocal && !scopeExcluded) {
     event.respondWith(
       caches.match(INDEX_HTML_URL, { cacheName: CACHE_NAME })
     );
